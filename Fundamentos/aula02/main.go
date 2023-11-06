33cb005050c53e8b7a32ee664a802c7b5776d4ee
Diogohs630 <diogo1.silva@alunos.ifsuldeminas.edu.br>
2023-11-06 12:24:29 -0300

    aula02

---
+++ Fundamentos/aula02/main.go
@@ -0,0 +1,16 @@
+package main
+
+const a = "Diogo"
+
+var (
+	b bool    = true
+	c int     = 20
+	d string  = "Diogo Henrique"
+	e float64 = 12.9
+)
+
+func main() {
+	println(a)
+	a := "X"
+	println(a)
+}
